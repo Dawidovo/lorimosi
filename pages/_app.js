e4115247f2fce58292ceea8344559a03692a832b
Dawidovo <davidpape@rocketmail.com>
2025-08-10 23:01:15 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/core/main.min.css';
+import '@fullcalendar/daygrid/main.min.css';
+import '@fullcalendar/timegrid/main.min.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
