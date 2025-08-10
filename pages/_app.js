5d5e684ad874751646ec62e4a4d1dae9d396bac6
Dawidovo <davidpape@rocketmail.com>
2025-08-10 22:38:12 +0200

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
