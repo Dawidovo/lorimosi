fd3cdc959909a8774d4346c246c67ca617bdbb05
Dawidovo <davidpape@rocketmail.com>
2025-08-11 21:45:13 +0200

    Delete pages/pages directory

--- pages/pages/test.js
+++
@@ -1,8 +0,0 @@
-export default function Test() {
-  return (
-    <div>
-      <p>Supabase URL: {process.env.NEXT_PUBLIC_SUPABASE_URL}</p>
-      <p>Anon Key: {process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? 'Key found' : 'No key found'}</p>
-    </div>
-  );
-}
