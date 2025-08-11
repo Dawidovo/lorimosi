2caf6aa1da029befc585956bf2d1c6dd2dcbc03c
Dawidovo <davidpape@rocketmail.com>
2025-08-11 23:18:56 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 // pages/_app.js
-import "@fullcalendar/core/index.css";
-import "@fullcalendar/daygrid/index.css";
-import "@fullcalendar/timegrid/index.css";
-import "../styles/globals.css"; // falls vorhanden – sonst Zeile löschen
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* FullCalendar CSS via CDN (passt zu v6.1.14) */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.14/main.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.14/main.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid@6.1.14/main.min.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
